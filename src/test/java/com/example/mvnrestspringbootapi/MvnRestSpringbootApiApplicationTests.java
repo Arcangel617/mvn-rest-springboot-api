fc564c57e8dc216e8eeb4db4d7d04182d6854990
Arcangel Artigue <artigue_arcangel@hotmail.com>
2020-10-12 20:57:46 -0300

    feat: first commit

---
+++ src/test/java/com/example/mvnrestspringbootapi/MvnRestSpringbootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvnrestspringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvnRestSpringbootApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
